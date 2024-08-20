c7e7d8643baa0180a2f8f0314c99ce00112282ef
KTellezE <Kevin.Tellez@axity.com>
2024-08-20 22:29:16 +0000

    Version modificada para singleton

--- Creacionales/Singleton/index.ts
+++ Creacionales/Singleton/index.ts
@@ -2,4 +2,9 @@
 import { EnrollmentService } from './EnrollmentService';
 import { BillingService } from './BillingService';
 
+const incriptionService = new EnrollmentService()
+const billingService = new BillingService()
+const contactMail = "Kevin.Tellez@axity.com"
 
+incriptionService.enrollStudent(contactMail)
+billingService.notifyPaymentDue(contactMail)
